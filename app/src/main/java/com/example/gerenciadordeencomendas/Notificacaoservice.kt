ac410b0382ff884ca69fdc696f6ec43be4492a77
Isaque Nogueira <109471754+IsaqueNogueira@users.noreply.github.com>
2022-12-17 12:31:14 -0300

    implementando notificação

--- app/src/main/java/com/example/gerenciadordeencomendas/Notificacaoservice.kt
+++ app/src/main/java/com/example/gerenciadordeencomendas/Notificacaoservice.kt
@@ -2,20 +2,21 @@ package com.example.gerenciadordeencomendas
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Intent
 import android.os.Build
-import android.os.Handler
 import android.os.IBinder
-import android.os.Looper
 import android.util.Log
 import androidx.core.app.NotificationCompat
-import androidx.lifecycle.*
+import androidx.lifecycle.LifecycleService
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import com.example.gerenciadordeencomendas.repository.Repository
+import com.example.gerenciadordeencomendas.ui.activity.EncomendasActivity
 import com.example.gerenciadordeencomendas.webcliente.model.RastreioWebClientMelhorEnvio
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import java.util.*
 
 private const val IDENTIFICADOR_DO_CANAL = "principal"
@@ -29,6 +30,9 @@ class Notificacaoservice : LifecycleService() {
     val rastreioWebClientMelhorRastreio by lazy {
         RastreioWebClientMelhorEnvio()
     }
+    companion object {
+        var id = 1
+    }
 
     override fun onBind(arg0: Intent?): IBinder? {
         super.onBind(arg0)
@@ -38,13 +42,12 @@ class Notificacaoservice : LifecycleService() {
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         super.onStartCommand(intent, flags, startId)
-        Log.e("TAG", "onStartCommand")
 
         val gerenciadorDeNotificacoes =
             applicationContext.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val nome = getString(R.string.default_web_client_id)
-            val descricao = getString(R.string.default_web_client_id)
+            val nome = getString(R.string.channel_name)
+            val descricao = getString(R.string.channel_description)
             val importancia = NotificationManager.IMPORTANCE_DEFAULT
             val canal = NotificationChannel(IDENTIFICADOR_DO_CANAL, nome, importancia)
             canal.description = descricao
@@ -54,15 +57,38 @@ class Notificacaoservice : LifecycleService() {
 
         fun foo() {
             verificaAtualizacaoRastreio().observe(this, Observer {
-                if (it != "Não atualizou") {
+                if (it != null) {
+
+                    val i = Intent(this, EncomendasActivity::class.java)
+                    var pendingIntent: PendingIntent? = null
+                    pendingIntent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                        PendingIntent.getActivity(
+                            this,
+                            0,
+                            i,
+                            PendingIntent.FLAG_MUTABLE
+                        )
+                    } else {
+                        PendingIntent.getActivity(
+                            this,
+                            0,
+                            i,
+                            PendingIntent.FLAG_ONE_SHOT
+                        )
+                    }
+
                     val notificacao = NotificationCompat.Builder(this, IDENTIFICADOR_DO_CANAL)
                         .setContentTitle(it)
                         .setContentText("Está em trânsito")
                         .setSmallIcon(R.drawable.logo)
                         .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                        .setContentIntent(pendingIntent)
+                        .setAutoCancel(true)
                         .build()
-                    gerenciadorDeNotificacoes.notify(1, notificacao)
-                }
+
+                    gerenciadorDeNotificacoes.notify(id, notificacao)
+
+                    }
             })
         }
         Timer().scheduleAtFixedRate(object : TimerTask() {
@@ -75,28 +101,29 @@ class Notificacaoservice : LifecycleService() {
 
         // START_STICKY serve para executar seu serviço até que você pare ele, é reiniciado automaticamente sempre que termina
         return START_STICKY
+
     }
 
 
     private fun verificaAtualizacaoRastreio(): LiveData<String> {
         val liveData = MutableLiveData<String>()
         repository.buscaTodasEncomendas().observe(this, Observer {
-            for (i in it) {
+            it.forEach() {
                 lifecycleScope.launch {
-                        rastreioWebClientMelhorRastreio.buscaRastreio(i.codigoRastreio)
+                        rastreioWebClientMelhorRastreio.buscaRastreio(it.codigoRastreio)
                             ?.let { rastreioEncontrado ->
                                 if (rastreioEncontrado.success == true) {
                                     val tamanhoEvent = rastreioEncontrado.data.events.size - 1
                                     val ultimoStatus =
                                         rastreioEncontrado.data.events.get(tamanhoEvent)
-                                    if (i.dataHoraApi != ultimoStatus.date) {
-                                        Log.i("TAG", "onCreate: Mudou")
-                                        liveData.value = i.nomePacote
+                                    if (it.dataHoraApi != ultimoStatus.date) {
+                                        liveData.value = it.nomePacote
                                     } else {
-                                        liveData.value = "Não atualizou"
+                                        liveData.value = null
                                     }
                                 }
                             }
+
                 }
             }
         })
--- app/src/main/java/com/example/gerenciadordeencomendas/fcm/GerenciadorEncomendasFirebaseMessagingService.kt
+++ app/src/main/java/com/example/gerenciadordeencomendas/fcm/GerenciadorEncomendasFirebaseMessagingService.kt
@@ -1,10 +1,68 @@
 package com.example.gerenciadordeencomendas.fcm
 
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.Ringtone
+import android.media.RingtoneManager
+import android.os.Build
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.example.gerenciadordeencomendas.R
+import com.example.gerenciadordeencomendas.ui.activity.EncomendasActivity
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
 private const val TAG = "Gerenciador FCM"
+
 class GerenciadorEncomendasFirebaseMessagingService : FirebaseMessagingService() {
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+        if (message.notification != null) {
+            Log.i(TAG, "onMessageReceived: ${message.notification!!.title}")
+            Log.i(TAG, "onMessageReceived: ${message.notification!!.body}")
+
+            val titulo = message.notification!!.title
+            val mensagem = message.notification!!.body
+            senNotification(titulo, mensagem)
+        }
+
+    }
+
+    @SuppressLint("UnspecifiedImmutableFlag")
+    private fun senNotification(titulo: String?, mensagem: String?) {
+
+        val intent = Intent(this, EncomendasActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+
+        val canal = getString(R.string.default_web_client_id)
+        val som = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+
+        val notification = NotificationCompat.Builder(this, canal)
+            .setSmallIcon(R.drawable.logo)
+            .setContentTitle(titulo)
+            .setContentText(mensagem)
+            .setSound(som)
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager =
+            applicationContext.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel =
+                NotificationChannel(canal, "canal", NotificationManager.IMPORTANCE_DEFAULT)
+            notificationManager.createNotificationChannel(channel)
+
+            notificationManager.notify(0, notification.build())
+        }
+    }
 
     override fun onNewToken(token: String) {
         super.onNewToken(token)
